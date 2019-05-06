80f51d1dd39818d0c3d248e3ae8ad3e01be6e795
Kota Tanaka <te11k.tanaka@gmail.com>
2019-05-06 20:00:36 +0900

    feat: カスタムフォントの導入・まとめてrequiredしてexportする

---
+++ src/fonts.js
@@ -0,0 +1,14 @@
+const genjuMenium = require('../assets/fonts/GenJyuuGothic-Medium.ttf')
+const genjuLight = require('../assets/fonts/GenJyuuGothic-Light.ttf')
+
+/**
+ * フォントをまとめてrequireしてexportしておく
+ * 
+ * @author tanakakota
+ */
+const fonts = {
+  'genju-medium': genjuMenium,
+  'genju-light': genjuLight,
+};
+
+export default fonts;
