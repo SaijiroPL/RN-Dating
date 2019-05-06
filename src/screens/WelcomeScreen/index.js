938b09fe40e0f5191e3f6ff288911d80a854e3b5
Kota Tanaka <te11k.tanaka@gmail.com>
2019-05-06 20:02:55 +0900

    feat: 初回説明スライドビューに画像配置・デザイン調整

--- src/screens/WelcomeScreen/index.js
+++ src/screens/WelcomeScreen/index.js
@@ -1,67 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, Image, Dimensions } from 'react-native';
+import { Text, View, ScrollView, Image } from 'react-native';
 import { Button } from 'react-native-elements';
 
+// from app
+import images from '../../images';
+import styles from './styles';
+
 // Constants
-const SCREEN_WIDTH = Dimensions.get('window').width;
 const SLIDE_DATA = [
   {
-    step: 'Step: 1',
     title: '日時の入力',
-    text: 'デート当日の日付と時間、移動手段を入力します',
-    uri: require('../../../assets/images/robot-dev.png')
+    description: 'デート当日の日付と時間、移動手段を入力します。',
+    uri: images.welcome01
   },
   {
-    step: 'Step: 2',
     title: '場所とピン付け',
-    text: 'マップで大まかな行動範囲を設定します',
-    uri: require('../../../assets/images/robot-dev.png')
+    description: 'マップで大まかな行動範囲を設定します。',
+    uri: images.welcome02
   },
   {
-    step: 'Step: 3',
     title: 'フリックでリストアップ',
-    text: 'デートスポット候補をフリックで選択します',
-    uri: require('../../../assets/images/robot-dev.png') },
-  {
-    step: 'Step: 4',
-    title: 'スポット厳選',
-    text: 'リストアップした候補を厳選します',
-    uri: require('../../../assets/images/robot-dev.png')
+    description: 'デートスポット候補をフリックで選択します。',
+    uri: images.welcome03
   },
   {
-    step: 'Step: 5',
+    title: 'スポット厳選',
+    description: 'リストアップした候補を厳選します。',
+    uri: images.welcome04
+  },
+  {
     title: '順番とプラン名',
-    text: '訪れる順番とデートプラン名を決定します',
-    uri: require('../../../assets/images/robot-dev.png')
+    description: '訪れる順番とデートプラン名を決定します。',
+    uri: images.welcome05
   },
 ];
 
 /**
  * ウェルカム画面
+ * 
+ * @author tanakakota
  */
-class WelcomeScreen extends React.Component {
-  /**
-   * 完了ボタン押下でホーム画面に遷移する
-   */
+export default class WelcomeScreen extends React.Component {
+
+  /** 完了ボタン押下でホーム画面に遷移する */
   onStartButtonPress = () => {
     this.props.navigation.navigate('main');
   }
 
   /**
    * 最後のページに完了ボタンを配置する
+   * 
    * @param index ページのインデックス
    */
   renderLastButton(index) {
     if (index === SLIDE_DATA.length - 1) {
       return (
         <View>
-          <Text style={styles.textStyle}>Let's make the plan!</Text>
           <Button
-            style={{
+            buttonStyle={{
+              backgroundColor: 'orange',
               paddingVertical: 10,
-              paddingHorizontal: 30
+              paddingHorizontal: 50
             }}
-            buttonStyle={{ backgroundColor: 'orange' }}
             title="完了"
             onPress={this.onStartButtonPress}
           />
@@ -78,21 +78,26 @@ class WelcomeScreen extends React.Component {
           key={index}
           style={styles.slideStyle}
         >
-          <View style={styles.containerStyle}>
-            <Text style={styles.textStyle}>{slide.step}</Text>
-            <Text style={styles.titleStyle}>{slide.title}</Text>
-            <Text style={styles.textStyle}>{slide.text}</Text>
+          {/* TODO 固定ヘッダーにする */}
+          <View style={styles.headerStyle}>
+            <Text style={styles.headerTextStyle}>プラン作成方法</Text>
           </View>
 
           <Image
-            style={{ flex: 2 }}
+            style={{ flex: 3 }}
             resizeMode="contain"
             source={slide.uri}
           />
-        
+
+          {/* TODO 画像と説明の間にボーダーを入れる */}
           <View style={styles.containerStyle}>
+            <Text style={styles.titleStyle}>{slide.title}</Text>
+            <Text style={styles.descriptionStyle}>{slide.description}</Text>
+          </View>
+        
+          <View style={styles.footerStyle}>
             {this.renderLastButton(index)}
-            <Text style={styles.textStyle}>{index + 1} / 5</Text>
+            <Text style={styles.descriptionStyle}>{index + 1} / 5</Text>
           </View>
         </View>
       )
@@ -112,26 +117,3 @@ class WelcomeScreen extends React.Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  slideStyle: {
-    flex: 1,
-    alignItems: 'center',
-    width: SCREEN_WIDTH
-  },
-  containerStyle: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  titleStyle: {
-    fontSize: 30,
-    padding: 10
-  },
-  textStyle: {
-    fontSize: 20,
-    padding: 5
-  }
-})
-
-export default WelcomeScreen;
