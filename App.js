bb28d7877ad350f46458f43a11a1307f26227920
Kota Tanaka <te11k.tanaka@gmail.com>
2019-05-06 20:01:55 +0900

    feat: ローカルリソースの読み込み処理

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { AppLoading, Asset, Font } from 'expo';
 
 // Navigator
 import AppNavigator from './src/navigation/AppNavigator';
 
+// from app
+import images from './src/images';
+import fonts from './src/fonts';
+
 export default class App extends React.Component {
+  static defaultProps = {
+    skipLoadingScreen: false,
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoadingComplete: false,
+    };
+  }
+
+  /** ローカルリソースの読み込み */
+  loadResourcesAsync = async () => {
+    await Asset.loadAsync(Object.keys(images).map(key => images[key]));
+    await Font.loadAsync(fonts);
+    return true;
+  }
+
   render() {
+    const { isLoadingComplete } = this.state;
+    const { skipLoadingScreen } = this.props;
+
+    // リソースの読み込みが終わるまではAppLoadingをrenderする
+    if (!isLoadingComplete && !skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this.loadResourcesAsync}
+          onError={error => console.warn(error)}
+          onFinish={() => this.setState({ isLoadingComplete: true })}
+        />
+      );
+    }
+
     return (
       <View style={styles.container}>
         <AppNavigator />
