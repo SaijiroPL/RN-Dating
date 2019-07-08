059132ea9b4fe1d16ef742d1e017022f194ee976
Kota Tanaka <te11k.tanaka@gmail.com>
2019-07-09 01:10:07 +0900

    refactor: Stylelintの設定カスタマイズ

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -8,6 +8,10 @@ module.exports = {
   rules: {
     indentation: 2,
     "string-quotes": "single",
-    "order/properties-alphabetical-order": true
+    "order/properties-alphabetical-order": true,
+    "selector-type-no-unknown": null,
+    "selector-type-case": null,
+    "font-family-no-missing-generic-family-keyword": null,
+    "rule-empty-line-before": null
   }
 };
