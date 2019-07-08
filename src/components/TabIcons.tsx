8582515e307795dd8c84260797d52286d8900f80
Kota Tanaka <te11k.tanaka@gmail.com>
2019-07-09 00:18:25 +0900

    refactor: Reactのimport文修正

--- src/components/TabIcons.tsx
+++ src/components/TabIcons.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 
 // from app
--- src/screens/TopScreen/Welcome.tsx
+++ src/screens/TopScreen/Welcome.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { Text, View, ScrollView, Image } from "react-native";
 import {
   NavigationParams,
