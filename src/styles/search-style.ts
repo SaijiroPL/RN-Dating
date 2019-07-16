10033fb0405bb2c039b369ef2f866061072069f7
Kota Tanaka <te11k.tanaka@gmail.com>
2019-07-17 00:35:16 +0900

    feat: 検索バーの作成

---
+++ src/styles/search-style.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+// from app
+import colors from "app/src/constants/colors";
+
+/**
+ * 検索画面のスタイリング
+ * @author kotatanaka
+ */
+export const searchStyle = StyleSheet.create({
+  container: {
+    backgroundColor: colors.backgroundColor
+  }
+});
