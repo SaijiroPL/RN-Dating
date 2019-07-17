ec98f425be8dfaee1bf6a4999b32dcb9a0bae7c2
Kota Tanaka <te11k.tanaka@gmail.com>
2019-07-18 00:49:04 +0900

    feat: マイプランタブでプラン詳細とコメント一覧のナビゲーション追加

--- src/navigators/MyPlanNavigator.ts
+++ src/navigators/MyPlanNavigator.ts
@@ -2,6 +2,8 @@ import { createStackNavigator, createAppContainer } from "react-navigation";
 
 // from app
 import MyPlanTopScreen from "app/src/screens/MyPlanScreen/MyPlanTopScreen";
+import PlanDetailScreen from "app/src/screens/DetailScreen/PlanDetailScreen";
+import CommentScreen from "app/src/screens/DetailScreen/CommentScreen";
 import appStyle from "app/src/styles/common-style";
 
 /**
@@ -15,6 +17,22 @@ const MyPlanNavigator = createStackNavigator(
       screen: MyPlanTopScreen,
       navigationOptions: () => ({
         headerTitle: "マイプラン",
+        headerTitleStyle: appStyle.defaultText
+      })
+    },
+    // デートプラン詳細画面
+    detail: {
+      screen: PlanDetailScreen,
+      navigationOptions: () => ({
+        headerTitle: "プラン詳細",
+        headerTitleStyle: appStyle.defaultText
+      })
+    },
+    // コメント一覧画面
+    comment: {
+      screen: CommentScreen,
+      navigationOptions: () => ({
+        headerTitle: "コメント",
         headerTitleStyle: appStyle.defaultText
       })
     }
