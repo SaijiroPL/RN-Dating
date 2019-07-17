80a54ebc8dd6e88580f034f93129b0b9a75653d8
Kota Tanaka <te11k.tanaka@gmail.com>
2019-07-18 00:41:24 +0900

    feat: マイプラン画面でデートプランリストを表示

--- src/screens/MyPlanScreen/MyPlanTopScreen.tsx
+++ src/screens/MyPlanScreen/MyPlanTopScreen.tsx
@@ -1,26 +1,70 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { Constants } from "expo";
 import {
   NavigationParams,
   NavigationScreenProp,
   NavigationState
 } from "react-navigation";
+import axiosBase from "axios";
 
 // from app
-import appStyle from "app/src/styles/common-style";
+import { PlanList, Error } from "app/src/constants/interfaces";
+import PlanCardList from "app/src/components/PlanCardList";
+import { myPlanStyle } from "app/src/styles/myplan-style";
 
 interface Props {
   navigation: NavigationScreenProp<NavigationState, NavigationParams>;
 }
 
+interface State {
+  plans: PlanList;
+  errors: Error;
+}
+
+const axios = axiosBase.create({
+  baseURL: Constants.manifest.extra.apiEndpoint + "/plans"
+});
+
 /**
  * マイプラン画面トップ
+ * @author kotatanaka
  */
 export default class MyPlanTopScreen extends React.Component<Props> {
+  public state: State = {
+    plans: { total: 0, plan_list: [] },
+    errors: { code: 0, message: "", detail_massage: [] }
+  };
+
+  componentDidMount() {
+    this.getPlanList();
+  }
+
+  /** デートプラン一覧取得 */
+  // TODO ユーザーID繋ぎこみ
+  getPlanList() {
+    axios
+      .get("", {
+        params: {
+          user_id: "hoge"
+        }
+      })
+      .then((response: { data: PlanList }) => {
+        this.setState({ plans: response.data });
+      })
+      .catch((error: Error) => {
+        this.setState({ errors: error });
+      });
+  }
+
   render() {
+    const { navigation } = this.props;
+    const { plans } = this.state;
+
     return (
-      <View style={appStyle.defaultContainer}>
-        <Text>マイプラン画面</Text>
+      <View style={myPlanStyle.container}>
+        <Text>作成したデートプランの数 {plans.total}</Text>
+        <PlanCardList navigation={navigation} planList={plans.plan_list} />
       </View>
     );
   }
---
+++ src/styles/myplan-style.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+// from app
+import colors from "app/src/constants/colors";
+
+/**
+ * マイプラン画面のスタイリング
+ * @author kotatanaka
+ */
+export const myPlanStyle = StyleSheet.create({
+  container: {
+    backgroundColor: colors.backgroundColor
+  }
+});
