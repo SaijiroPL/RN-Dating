f155e8e1303f66ef8985b07b62d40da3dd941119
Kota Tanaka <te11k.tanaka@gmail.com>
2019-07-24 19:46:54 +0900

    feat: ユーザー情報取得API繋ぎこみ

--- src/screens/ProfileScreen/ProfileTopScreen.tsx
+++ src/screens/ProfileScreen/ProfileTopScreen.tsx
@@ -1,26 +1,76 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { Constants } from "expo";
 import {
   NavigationParams,
   NavigationScreenProp,
   NavigationState
 } from "react-navigation";
+import axiosBase from "axios";
 
 // from app
-import appStyle from "app/src/styles/common-style";
+import { UserDetail, BadRequestError } from "app/src/constants/interfaces";
+import UserProfile from "app/src/components/UserProfile";
+import { profileStyle } from "app/src/styles/profile-screen-style";
 
 interface Props {
   navigation: NavigationScreenProp<NavigationState, NavigationParams>;
 }
 
+interface State {
+  user: UserDetail;
+  errors: BadRequestError;
+}
+
+const axios = axiosBase.create({
+  baseURL: Constants.manifest.extra.apiEndpoint + "/users"
+});
+
 /**
  * プロフィール画面トップ
+ * @author kotatanaka
  */
-export default class ProfileTopScreen extends React.Component<Props> {
+export default class HomeTopScreen extends React.Component<Props, State> {
+  public state: State = {
+    user: {
+      user_id: "",
+      name: "",
+      sex: "",
+      age: 0,
+      area: "",
+      mail_address: "",
+      user_attr: "",
+      user_image_url: "",
+      plan_count: 1
+    },
+    errors: { code: 0, message: "", detail_massage: [] }
+  };
+
+  componentDidMount() {
+    this.getUserDetail();
+  }
+
+  /** ユーザー詳細取得 */
+  getUserDetail() {
+    // TODO ユーザーID繋ぎこみ
+    const userId = "ab097f77-9f49-4b5e-9b83-e5419bc11d0d";
+    axios
+      .get("" + "/" + userId)
+      .then((response: { data: UserDetail }) => {
+        this.setState({ user: response.data });
+      })
+      .catch((error: BadRequestError) => {
+        this.setState({ errors: error });
+      });
+  }
+
   render() {
+    const { navigation } = this.props;
+    const { user } = this.state;
+
     return (
-      <View style={appStyle.defaultContainer}>
-        <Text>設定画面</Text>
+      <View style={profileStyle.container}>
+        <UserProfile navigation={navigation} user={user} />
       </View>
     );
   }
