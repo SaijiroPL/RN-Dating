b8bddcb3520e69e3dd038ad7235f8ef0ae6778cd
Kota Tanaka <te11k.tanaka@gmail.com>
2019-07-31 00:42:35 +0900

    refactor: AppTopScreenをFCにしてHooks導入

--- src/screens/TopScreen/AppTopScreen.tsx
+++ src/screens/TopScreen/AppTopScreen.tsx
@@ -1,10 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View, Image } from "react-native";
-import {
-  NavigationParams,
-  NavigationScreenProp,
-  NavigationState
-} from "react-navigation";
+import { useNavigation } from "react-navigation-hooks";
 import { Button, Input } from "react-native-elements";
 import { FontAwesome } from "@expo/vector-icons";
 
@@ -15,17 +11,6 @@ import layout from "app/src/constants/layout";
 import colors from "app/src/constants/colors";
 import { topStyle } from "app/src/styles/top-screen-style";
 
-interface Props {
-  navigation: NavigationScreenProp<NavigationState, NavigationParams>;
-}
-
-interface State {
-  screenPhase: number;
-  mailAddress: string;
-  password: string;
-  rePassword: string;
-}
-
 // 仮置き定数
 const loginUserId = "ab097f77-9f49-4b5e-9b83-e5419bc11d0d";
 
@@ -33,36 +18,33 @@ const loginUserId = "ab097f77-9f49-4b5e-9b83-e5419bc11d0d";
  * 初回起動時の画面
  * @author kotatanaka
  */
-export default class AppTopScreen extends React.Component<Props, State> {
-  public state: State = {
-    // [0]初回画面 [1]メールアドレスログイン画面 [2]新規登録画面
-    screenPhase: 0,
-    mailAddress: "",
-    password: "",
-    rePassword: ""
-  };
+const AppTopScreen: React.FC = () => {
+  const { navigate } = useNavigation();
+
+  // [0]初回画面 [1]メールアドレスログイン画面 [2]新規登録画面
+  const [screenPhase, setScreenPhase] = useState(0);
+  const [mailAddress, setMailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmpassword] = useState("");
 
   /** Facebookログインボタン押下時の処理 */
-  onFacebookButtonPress = () => {
-    const { navigation } = this.props;
-    navigation.navigate("welcome");
+  const onFacebookButtonPress = () => {
+    navigate("welcome");
   };
 
   /** メールアドレスログインボタン押下時の処理 */
-  onSignInButtonPress = () => {
-    const { navigation } = this.props;
+  const onSignInButtonPress = () => {
     globals.loginUser.id = loginUserId;
-    navigation.navigate("main");
+    navigate("main");
   };
 
   /** 新規登録ボタン押下時の処理 */
-  onSignUpButtonPress = () => {
-    const { navigation } = this.props;
-    navigation.navigate("welcome");
+  const onSignUpButtonPress = () => {
+    navigate("welcome");
   };
 
   /** 初期画面を描画する */
-  renderTopScreen() {
+  const renderTopScreen = () => {
     return (
       <View>
         <FontAwesome.Button
@@ -71,107 +53,95 @@ export default class AppTopScreen extends React.Component<Props, State> {
           backgroundColor={colors.facebookColor}
           borderRadius={30}
           iconStyle={{ marginLeft: 30 }}
-          onPress={this.onFacebookButtonPress}
+          onPress={onFacebookButtonPress}
         >
           Facebookでログイン
         </FontAwesome.Button>
-        <Text
-          style={topStyle.link}
-          onPress={() => this.setState({ screenPhase: 1 })}
-        >
+        <Text style={topStyle.link} onPress={() => setScreenPhase(1)}>
           メールアドレスでログイン
         </Text>
-        <Text
-          style={topStyle.link}
-          onPress={() => this.setState({ screenPhase: 2 })}
-        >
+        <Text style={topStyle.link} onPress={() => setScreenPhase(2)}>
           新規登録はこちら
         </Text>
       </View>
     );
-  }
+  };
 
   /** メールアドレスログイン画面を描画する */
-  renderSignInScreen() {
-    const { mailAddress, password } = this.state;
-
+  const renderSignInScreen = () => {
     return (
       <View>
         <Input
           placeholder="メールアドレスを入力"
-          onChangeText={mailAddress => this.setState({ mailAddress })}
+          onChangeText={mailAddress => setMailAddress(mailAddress)}
           value={mailAddress}
           containerStyle={topStyle.inputForm}
         />
         <Input
           placeholder="パスワードを入力"
-          onChangeText={password => this.setState({ password })}
+          onChangeText={password => setPassword(password)}
           value={password}
           containerStyle={topStyle.inputForm}
         />
         <Button
           buttonStyle={topStyle.completeButton}
           title="ログイン"
-          onPress={this.onSignInButtonPress}
+          onPress={onSignInButtonPress}
         />
       </View>
     );
-  }
+  };
 
   /** 新規登録画面を描画する */
-  renderSignUpScreen() {
-    const { mailAddress, password, rePassword } = this.state;
-
+  const renderSignUpScreen = () => {
     return (
       <View>
         <Input
           placeholder="メールアドレスを入力"
-          onChangeText={mailAddress => this.setState({ mailAddress })}
+          onChangeText={mailAddress => setMailAddress(mailAddress)}
           value={mailAddress}
           containerStyle={topStyle.inputForm}
         />
         <Input
           placeholder="パスワードを入力"
-          onChangeText={password => this.setState({ password })}
+          onChangeText={password => setPassword(password)}
           value={password}
           containerStyle={topStyle.inputForm}
         />
         <Input
           placeholder="パスワードを再入力"
-          onChangeText={rePassword => this.setState({ rePassword })}
-          value={rePassword}
+          onChangeText={confirmPassword => setConfirmpassword(confirmPassword)}
+          value={confirmPassword}
           containerStyle={topStyle.inputForm}
         />
         <Button
           buttonStyle={topStyle.completeButton}
           title="新規登録"
-          onPress={this.onSignUpButtonPress}
+          onPress={onSignUpButtonPress}
         />
       </View>
     );
-  }
+  };
 
-  render() {
-    const { screenPhase } = this.state;
-
-    return (
-      <View style={topStyle.topContainer}>
-        <View style={topStyle.emptySpace} />
-        <View style={topStyle.topImage}>
-          <Image
-            resizeMode="contain"
-            source={images.logo}
-            style={{ flex: 1 }}
-            width={layout.window.width * 0.8}
-          />
-          <Text style={topStyle.welcomeText}>1Dateへようこそ</Text>
-        </View>
-        <View style={topStyle.linkGroup}>
-          {screenPhase === 0 && this.renderTopScreen()}
-          {screenPhase === 1 && this.renderSignInScreen()}
-          {screenPhase === 2 && this.renderSignUpScreen()}
-        </View>
+  return (
+    <View style={topStyle.topContainer}>
+      <View style={topStyle.emptySpace} />
+      <View style={topStyle.topImage}>
+        <Image
+          resizeMode="contain"
+          source={images.logo}
+          style={{ flex: 1 }}
+          width={layout.window.width * 0.8}
+        />
+        <Text style={topStyle.welcomeText}>1Dateへようこそ</Text>
       </View>
-    );
-  }
-}
+      <View style={topStyle.linkGroup}>
+        {screenPhase === 0 && renderTopScreen()}
+        {screenPhase === 1 && renderSignInScreen()}
+        {screenPhase === 2 && renderSignUpScreen()}
+      </View>
+    </View>
+  );
+};
+
+export default AppTopScreen;
