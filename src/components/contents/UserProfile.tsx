176824b9ef420966252458bd4f7b0b62b7b6d5a8
Kota Tanaka <te11k.tanaka@gmail.com>
2019-08-06 02:44:04 +0900

    feat: プロフィール画面のスタイリング

--- src/components/contents/UserProfile.tsx
+++ src/components/contents/UserProfile.tsx
@@ -1,9 +1,12 @@
 import React from "react";
-import { Content, Thumbnail, Text } from "native-base";
+import { View } from "react-native";
+import { Container, Content, Thumbnail, Text, Left, Right } from "native-base";
+import { Avatar } from "react-native-elements";
 
 // from app
 import { UserDetail } from "app/src/constants/interfaces";
 import images from "app/src/constants/images";
+import { profileStyle } from "app/src/styles/profile-screen-style";
 
 interface Props {
   user: UserDetail;
@@ -15,11 +18,23 @@ interface Props {
  */
 const UserProfile: React.FC<Props> = ({ user }) => {
   return (
-    <Content>
-      <Thumbnail square large source={images.noUserImage} />
-      <Text>{user.name}</Text>
-      <Text note>{user.user_id}</Text>
-    </Content>
+    <View style={profileStyle.userContainer}>
+      <Thumbnail large source={images.noUserImage} />
+      <Text style={profileStyle.nameText}>{user.name}</Text>
+      <Text note style={profileStyle.nameText}>
+        {user.user_id}
+      </Text>
+      <View style={profileStyle.followCounts}>
+        <View style={profileStyle.followCountItem}>
+          <Text style={profileStyle.countText}>フォロー</Text>
+          <Text style={profileStyle.countText}>{user.follow_count}</Text>
+        </View>
+        <View style={profileStyle.followCountItem}>
+          <Text style={profileStyle.countText}>フォロワー</Text>
+          <Text style={profileStyle.countText}>{user.follower_count}</Text>
+        </View>
+      </View>
+    </View>
   );
 };
 
--- src/styles/profile-screen-style.ts
+++ src/styles/profile-screen-style.ts
@@ -13,7 +13,29 @@ export const profileStyle = StyleSheet.create({
     flex: 1,
     justifyContent: "center"
   },
+  userContainer: {
+    alignItems: "center"
+  },
+  followCounts: {
+    alignContent: "space-around",
+    flexDirection: "row",
+    marginTop: 30
+  },
+  followCountItem: {
+    alignItems: "center",
+    flexDirection: "column",
+    margin: 30
+  },
   settingFab: {
     backgroundColor: colors.tintColor
+  },
+  nameText: {
+    color: colors.textTintColor,
+    fontFamily: "genju-medium",
+    marginTop: 20
+  },
+  countText: {
+    color: colors.textTintColor,
+    fontFamily: "genju-medium"
   }
 });
