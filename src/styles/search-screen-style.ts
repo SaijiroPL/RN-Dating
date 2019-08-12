ec390c3105eda94469ef57e32e1090083ebd5b73
Kota Tanaka <te11k.tanaka@gmail.com>
2019-08-12 15:08:03 +0900

    feat: 検索バーのレイアウト調整

--- src/styles/search-screen-style.ts
+++ src/styles/search-screen-style.ts
@@ -10,5 +10,22 @@ import Colors from "app/src/constants/Colors";
 export const searchStyle = StyleSheet.create({
   container: {
     backgroundColor: Colors.backgroundColor
+  },
+  searchBar: {
+    backgroundColor: "white",
+    shadowColor: "#ccc",
+    shadowOffset: {
+      height: 1,
+      width: 1
+    },
+    shadowOpacity: 1,
+    shadowRadius: 2
+  },
+  searchInput: {
+    backgroundColor: "#eee"
+  },
+  planCount: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1
   }
 });
