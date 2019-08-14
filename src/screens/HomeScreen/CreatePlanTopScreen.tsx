7b39ffe0fb206f29a261ddcc57e8eb0896421c69
Kota Tanaka <te11k.tanaka@gmail.com>
2019-08-14 15:30:00 +0900

    feat: プラン作成フローのナビゲーション

--- src/screens/HomeScreen/CreatePlanTopScreen.tsx
+++ src/screens/HomeScreen/CreatePlanTopScreen.tsx
@@ -1,16 +1,24 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { useNavigation } from "react-navigation-hooks";
 
 // from app
+import CompleteButton from "app/src/components/buttons/CompleteButton";
 import { homeStyle } from "app/src/styles/home-screen-style";
 
 /**
  * プラン作成画面トップ
  */
 const CreatePlanTopScreen: React.FC = () => {
+  const { navigate } = useNavigation();
+
+  const onCompleteButtonPress = () => {
+    navigate("map");
+  };
+
   return (
-    <View style={homeStyle.container}>
-      <Text>プラン作成</Text>
+    <View style={homeStyle.createPlanContainer}>
+      <CompleteButton title="決定" onPress={onCompleteButtonPress} />
     </View>
   );
 };
