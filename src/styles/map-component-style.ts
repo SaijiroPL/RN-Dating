75414d4e7b9d4e88ff452a3e8f3aa838406f4a56
Kota Tanaka <te11k.tanaka@gmail.com>
2019-08-18 19:37:10 +0900

    feat: プラン作成の範囲指定画面でマップを表示

---
+++ src/styles/map-component-style.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+// from app
+import Colors from "app/src/constants/Colors";
+import Layout from "app/src/constants/Layout";
+
+/**
+ * マップ関連関連コンポーネントのスタイリング
+ * @author kotatanaka
+ */
+export const mapViewStyle = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center"
+  }
+});
