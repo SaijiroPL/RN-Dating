3406a2162fd3d88d54b151d5e3ab6dbb13f6081d
Kota Tanaka <te11k.tanaka@gmail.com>
2019-08-30 02:26:01 +0900

    feat: 通知一覧画面にプルリロード機能追加

--- src/screens/NotificationScreen/NotificationLikeScreen.tsx
+++ src/screens/NotificationScreen/NotificationLikeScreen.tsx
@@ -1,18 +1,34 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { ScrollView, Text, View } from "react-native";
 
 // from app
 import { appTextStyle } from "app/src/styles/general-style";
+import { RefreshSpinner } from "app/src/components/Spinners";
 import notificationScreenStyle from "app/src/styles/notification-screen-style";
 
 /**
  * お気に入り通知一覧画面
+ * @author kotatanaka
  */
 const NotificationLikeScreen: React.FC = () => {
+  const [notifications, setNotifications] = useState<Array<any>>([]);
+  const [isRefreshing, setRefreshing] = useState<boolean>(false);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setRefreshing(false);
+  };
+
   return (
-    <View style={notificationScreenStyle.container}>
-      <Text style={appTextStyle.defaultText}>お気に入り通知はありません。</Text>
-    </View>
+    <ScrollView refreshControl={RefreshSpinner(isRefreshing, onRefresh)}>
+      <View style={notificationScreenStyle.container}>
+        {!notifications.length && (
+          <Text style={appTextStyle.defaultText}>
+            お気に入り通知はありません。
+          </Text>
+        )}
+      </View>
+    </ScrollView>
   );
 };
 
