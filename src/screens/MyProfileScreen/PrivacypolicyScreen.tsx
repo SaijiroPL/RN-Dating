7c7670f83118177d6793b50a0d756acd650bb23d
Kota Tanaka <te11k.tanaka@gmail.com>
2019-08-31 04:34:17 +0900

    feat: WebView読み込み中はぐるぐる表示

--- src/screens/MyProfileScreen/PrivacypolicyScreen.tsx
+++ src/screens/MyProfileScreen/PrivacypolicyScreen.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { WebView } from "react-native";
+
+// from app
+import { LoadingSpinner } from "app/src/components/Spinners";
 
 /**
  * プライバシーポリシー画面
@@ -11,6 +14,8 @@ const PrivacypolicyScreen: React.FC = () => {
       // TODO Webページを実装したらURLを置き換える
       source={{ uri: "https://github.com/facebook/react-native" }}
       style={{ marginTop: 20 }}
+      renderLoading={() => LoadingSpinner}
+      startInLoadingState={true}
     />
   );
 };
--- src/screens/MyProfileScreen/TermsScreen.tsx
+++ src/screens/MyProfileScreen/TermsScreen.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { WebView } from "react-native";
 
+// from app
+import { LoadingSpinner } from "app/src/components/Spinners";
+
 /**
  * 利用規約画面
  * @author itsukiyamada
@@ -11,6 +14,8 @@ const TermsScreen: React.FC = () => {
       // TODO Webページを実装したらURLを置き換える
       source={{ uri: "https://github.com/facebook/react-native" }}
       style={{ marginTop: 20 }}
+      renderLoading={() => LoadingSpinner}
+      startInLoadingState={true}
     />
   );
 };
