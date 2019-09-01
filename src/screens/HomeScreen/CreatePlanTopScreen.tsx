92b4e3a38c8a2c2172969bdc9ff4ebfb0e6b8f6f
Kota Tanaka <te11k.tanaka@gmail.com>
2019-09-01 13:25:32 +0900

    refactor: プラン作成トップ画面の交通手段選択ボタンの変更

--- src/screens/HomeScreen/CreatePlanTopScreen.tsx
+++ src/screens/HomeScreen/CreatePlanTopScreen.tsx
@@ -4,6 +4,7 @@ import { useNavigation } from "react-navigation-hooks";
 import { Button, Text } from "native-base";
 
 // from app
+import SelectButton from "app/src/components/buttons/SelectButton";
 import DatePicker from "app/src/components/contents/DatePicker";
 import CompleteFooterButton from "app/src/components/buttons/CompleteFooterButton";
 import { getToday } from "app/src/utils/DateUtil";
@@ -39,54 +40,35 @@ const CreatePlanTopScreen: React.FC = () => {
     return transportationList;
   };
 
-  /**
-   * 移動手段ボタンの一つを描画する
-   * @param value state
-   * @param setValue state更新関数
-   * @param buttonName ボタン表示名
-   */
-  const renderTransportatoinButton = (
-    value: boolean,
-    setValue: React.Dispatch<React.SetStateAction<boolean>>,
-    buttonName: string
-  ) => {
-    if (!value) {
-      return (
-        <Button
-          small
-          light
-          style={createPlanTopScreenStyle.selectButtonInactive}
-          onPress={() => setValue(true)}
-        >
-          <Text style={createPlanTopScreenStyle.selectButtonInactiveText}>
-            {buttonName}
-          </Text>
-        </Button>
-      );
-    } else {
-      return (
-        <Button
-          small
-          style={createPlanTopScreenStyle.selectButtonActive}
-          onPress={() => setValue(false)}
-        >
-          <Text style={createPlanTopScreenStyle.selectButtonActiveText}>
-            {buttonName}
-          </Text>
-        </Button>
-      );
-    }
-  };
-
   /** 移動手段選択ボタンを描画する */
   const renderTransportationButtonGroup = () => {
     return (
       <View style={createPlanTopScreenStyle.formGroup}>
         <Text style={createPlanTopScreenStyle.itemTitleText}>移動手段</Text>
-        {renderTransportatoinButton(car, setCar, "車")}
-        {renderTransportatoinButton(train, setTrain, "電車")}
-        {renderTransportatoinButton(bus, setBus, "バス")}
-        {renderTransportatoinButton(walk, setWalk, "徒歩")}
+        <SelectButton
+          value={car}
+          setValue={setCar}
+          reversible
+          buttonName={"車"}
+        />
+        <SelectButton
+          value={train}
+          setValue={setTrain}
+          reversible
+          buttonName={"電車"}
+        />
+        <SelectButton
+          value={bus}
+          setValue={setBus}
+          reversible
+          buttonName={"バス"}
+        />
+        <SelectButton
+          value={walk}
+          setValue={setWalk}
+          reversible
+          buttonName={"徒歩"}
+        />
       </View>
     );
   };
