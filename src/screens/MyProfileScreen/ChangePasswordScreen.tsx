09bd4685e518e6303641e92b0112142018fed35b
山田樹 <iy.s_s_c3s@icloud.com>
2019-09-03 08:26:50 +0900

    feat: パスワード変更画面作成、若干感覚が変

--- src/screens/MyProfileScreen/ChangePasswordScreen.tsx
+++ src/screens/MyProfileScreen/ChangePasswordScreen.tsx
@@ -1,15 +1,44 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { useNavigation } from "react-navigation-hooks";
+import CompleteButton from "app/src/components/buttons/CompleteButton";
+import appStyle, { appTextStyle } from "app/src/styles/general-style";
+
+import { Container, Content, Form, Item, Input, Label } from "native-base";
 
 /**
  * パスワード変更画面
  * @author itsukiyamada
  */
 const ChangePasswordScreen: React.FC = () => {
+  const { navigate } = useNavigation();
+
+  const onCompleteButtonPress = () => {
+    navigate("top");
+  };
+
   return (
-    <View>
-      <Text>パスワード変更</Text>
-    </View>
+    <Container>
+      <Content>
+        <Form>
+          <Item floatingLabel>
+            <Label>現在のパスワード</Label>
+            <Input />
+          </Item>
+          <Item floatingLabel>
+            <Label>新しいパスワード</Label>
+            <Input />
+          </Item>
+          <Item floatingLabel last>
+            <Label>新しいパスワードの確認</Label>
+            <Input />
+          </Item>
+          <View style={appStyle.standardContainer}>
+            <CompleteButton title="完了" onPress={onCompleteButtonPress} />
+          </View>
+        </Form>
+      </Content>
+    </Container>
   );
 };
 
