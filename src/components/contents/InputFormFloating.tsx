7f078c4be2d2785a109e34fa9fecb9b0153ea186
Kota Tanaka <te11k.tanaka@gmail.com>
2019-09-04 01:49:03 +0900

    feat: ラベルが浮くカスタム入力フォームの作成

---
+++ src/components/contents/InputFormFloating.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Input, Item, Label } from "native-base";
+
+// from app
+import Layout from "app/src/constants/Layout";
+
+interface Props {
+  label: string;
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+}
+
+/**
+ * 入力中にラベルが浮く入力フォーム
+ * @author kotatanaka
+ */
+const IuputFormFloating: React.FC<Props> = (props: Props) => {
+  const { label, value, setValue } = props;
+
+  if (value === "") {
+    return (
+      <Item floatingLabel style={{ width: Layout.window.width * 0.9 }}>
+        <Label>{label}</Label>
+        <Input onChangeText={value => setValue(value)} value={value} />
+      </Item>
+    );
+  }
+
+  return (
+    <Item floatingLabel success style={{ width: Layout.window.width * 0.9 }}>
+      <Label>{label}</Label>
+      <Input onChangeText={value => setValue(value)} value={value} />
+    </Item>
+  );
+};
+
+export default IuputFormFloating;
