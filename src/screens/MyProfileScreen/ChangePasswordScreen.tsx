3de7f59a93a361f7344656eaab6ef5af6985f897
Kota Tanaka <te11k.tanaka@gmail.com>
2019-09-04 01:50:41 +0900

    feat: 入力フォームをカスタマイズ・未入力がある場合完了不可

--- src/screens/MyProfileScreen/ChangePasswordScreen.tsx
+++ src/screens/MyProfileScreen/ChangePasswordScreen.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
+import { Form } from "native-base";
 import { useNavigation } from "react-navigation-hooks";
-import CompleteButton from "app/src/components/buttons/CompleteButton";
-import appStyle, { appTextStyle } from "app/src/styles/general-style";
 
-import { Container, Content, Form, Item, Input, Label } from "native-base";
+// from app
+import InputFormFloating from "app/src/components/contents/InputFormFloating";
+import CompleteButton from "app/src/components/buttons/CompleteButton";
+import { isEmpty } from "app/src/utils/CheckUtil";
+import appStyle from "app/src/styles/general-style";
 
 /**
  * パスワード変更画面
@@ -13,32 +16,54 @@ import { Container, Content, Form, Item, Input, Label } from "native-base";
 const ChangePasswordScreen: React.FC = () => {
   const { navigate } = useNavigation();
 
+  const [currentPassword, setCurrentPassword] = useState<string>("");
+  const [newPassword, setNewPassword] = useState<string>("");
+  const [confirmNewPassword, setConfirmNewPassword] = useState<string>("");
+
+  /** 完了ボタン押下時の処理 */
   const onCompleteButtonPress = () => {
+    // TODO パスワード変更APIを叩く
     navigate("top");
   };
 
+  /**
+   * 完了ボタンを描画する
+   * 未入力がある場合は押せないようにする
+   */
+  const renderCompleteButton = () => {
+    if (
+      isEmpty(currentPassword) ||
+      isEmpty(newPassword) ||
+      isEmpty(confirmNewPassword)
+    ) {
+      return <CompleteButton title="完了" disabled />;
+    }
+
+    return <CompleteButton title="完了" onPress={onCompleteButtonPress} />;
+  };
+
   return (
-    <Container>
-      <Content>
-        <Form>
-          <Item floatingLabel>
-            <Label>現在のパスワード</Label>
-            <Input />
-          </Item>
-          <Item floatingLabel>
-            <Label>新しいパスワード</Label>
-            <Input />
-          </Item>
-          <Item floatingLabel last>
-            <Label>新しいパスワードの確認</Label>
-            <Input />
-          </Item>
-          <View style={appStyle.standardContainer}>
-            <CompleteButton title="完了" onPress={onCompleteButtonPress} />
-          </View>
-        </Form>
-      </Content>
-    </Container>
+    <View style={appStyle.standardContainer}>
+      <Form style={{ flex: 3, justifyContent: "center" }}>
+        <InputFormFloating
+          label="現在のパスワード"
+          value={currentPassword}
+          setValue={setCurrentPassword}
+        />
+        <InputFormFloating
+          label="新しいパスワード"
+          value={newPassword}
+          setValue={setNewPassword}
+        />
+        <InputFormFloating
+          label="新しいパスワードの確認"
+          value={confirmNewPassword}
+          setValue={setConfirmNewPassword}
+        />
+      </Form>
+      <View style={appStyle.standardContainer}>{renderCompleteButton()}</View>
+      <View style={appStyle.emptySpace} />
+    </View>
   );
 };
 
