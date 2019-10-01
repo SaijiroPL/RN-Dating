d65d91df5f8b2bffdfdb0bd9c111d21fd281a08c
Kota Tanaka <te11k.tanaka@gmail.com>
2019-10-01 19:43:51 +0900

    refactor: 現状の検索バーをコンポーネント化

---
+++ src/components/contents/SearchFormBar.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { SearchBar } from "react-native-elements";
+import { Ionicons } from "@expo/vector-icons";
+
+// from app
+import Colors from "app/src/constants/Colors";
+
+interface Props {
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+}
+
+/**
+ * 検索フォームバー
+ * @author kotatanaka
+ */
+const SearchFormBar: React.FC<Props> = (props: Props) => {
+  const { value, setValue } = props;
+  return (
+    <SearchBar
+      placeholder="検索"
+      round
+      lightTheme
+      searchIcon={
+        <Ionicons name="ios-search" size={26} color={Colors.textTintColor} />
+      }
+      clearIcon={
+        <Ionicons name="ios-close" size={26} color={Colors.textTintColor} />
+      }
+      onChangeText={value => setValue(value)}
+      onClear={() => setValue("")}
+      value={value}
+      containerStyle={thisStyle.searchBar}
+      inputContainerStyle={thisStyle.searchInput}
+    />
+  );
+};
+
+/** スタイリング */
+const thisStyle = StyleSheet.create({
+  searchBar: {
+    backgroundColor: "white",
+    shadowColor: "#ccc",
+    shadowOffset: {
+      height: 1,
+      width: 1
+    },
+    shadowOpacity: 1,
+    shadowRadius: 2
+  },
+  searchInput: {
+    backgroundColor: "#eee"
+  }
+});
+
+export default SearchFormBar;
--- src/screens/SearchScreen/SearchTopScreen.tsx
+++ src/screens/SearchScreen/SearchTopScreen.tsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
-import { SearchBar } from "react-native-elements";
 import axios, { CancelTokenSource } from "axios";
-import { Ionicons } from "@expo/vector-icons";
 
 // from app
 import { IPlanList } from "app/src/interfaces/api/Plan";
@@ -10,6 +8,7 @@ import { IApiError } from "app/src/interfaces/api/Error";
 import Colors from "app/src/constants/Colors";
 import { API_ENDPOINT } from "app/src/constants/Api";
 import { LoadingSpinner, RefreshSpinner } from "app/src/components/Spinners";
+import SearchFormBar from "app/src/components/contents/SearchFormBar";
 import PlanCardList from "app/src/components/lists/PlanCardList";
 import { handleError } from "app/src/utils/ApiUtil";
 import appTextStyle from "app/src/styles/GeneralTextStyle";
@@ -76,36 +75,13 @@ const SearchTopScreen: React.FC = () => {
     setRefreshing(false);
   };
 
-  /** 検索バーを描画する */
-  // TODO 入力文字のクリア
-  const renderSearchBar = () => {
-    return (
-      <SearchBar
-        placeholder="検索"
-        round
-        lightTheme
-        searchIcon={
-          <Ionicons name="ios-search" size={26} color={Colors.textTintColor} />
-        }
-        clearIcon={
-          <Ionicons name="ios-close" size={26} color={Colors.textTintColor} />
-        }
-        onChangeText={searchWord => updateSearchWord(searchWord)}
-        onClear={() => updateSearchWord("")}
-        value={searchWord}
-        containerStyle={thisStyle.searchBar}
-        inputContainerStyle={thisStyle.searchInput}
-      />
-    );
-  };
-
   if (isLoading) {
     return LoadingSpinner;
   }
 
   return (
     <View style={thisStyle.container}>
-      {renderSearchBar()}
+      <SearchFormBar value={searchWord} setValue={setSearchWord} />
       <View style={thisStyle.planCount}>
         <Text style={appTextStyle.countText}>検索結果: {plans.total} 件</Text>
       </View>
@@ -120,19 +96,6 @@ const SearchTopScreen: React.FC = () => {
 const thisStyle = StyleSheet.create({
   container: {
     backgroundColor: Colors.backgroundColor
-  },
-  searchBar: {
-    backgroundColor: "white",
-    shadowColor: "#ccc",
-    shadowOffset: {
-      height: 1,
-      width: 1
-    },
-    shadowOpacity: 1,
-    shadowRadius: 2
-  },
-  searchInput: {
-    backgroundColor: "#eee"
   },
   planCount: {
     borderBottomColor: "#ccc",
