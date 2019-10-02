e76b2c7d9a53d98253c7fdd14593ff227087dbb1
Kota Tanaka <te11k.tanaka@gmail.com>
2019-10-02 16:52:21 +0900

    feat: WebViewのURLを定数に追加

--- src/constants/Url.ts
+++ src/constants/Url.ts
@@ -1,29 +1,40 @@
 import Constants from "expo-constants";
 
-const HOST = Constants.manifest.extra.apiEndpoint;
+const API_HOST = Constants.manifest.extra.apiEndpoint;
+const WEB_HOST = Constants.manifest.extra.webEndpoint;
 
 /**
  * APIエンドポイント
  */
 export const API_ENDPOINT = {
   // ユーザー登録
-  USERS: HOST + "/users",
+  USERS: API_HOST + "/users",
   // ユーザー詳細取得, プロフィール編集
-  USER: HOST + "/users/$1",
+  USER: API_HOST + "/users/$1",
   // メールアドレスでログイン
-  USERS_LOGIN: HOST + "/users/login",
+  USERS_LOGIN: API_HOST + "/users/login",
   // フォローリスト取得
-  USER_FOLLOWS: HOST + "/users/$1/follows",
+  USER_FOLLOWS: API_HOST + "/users/$1/follows",
   // フォロワーリスト取得
-  USER_FOLLOWERS: HOST + "/users/$1/followers",
+  USER_FOLLOWERS: API_HOST + "/users/$1/followers",
   // デートプラン一覧取得
-  PLANS: HOST + "/plans",
+  PLANS: API_HOST + "/plans",
   // デートプラン検索
-  PLANS_SEARCH: HOST + "/plans/search",
+  PLANS_SEARCH: API_HOST + "/plans/search",
   // デートプラン詳細取得, デートプラン編集, デートプラン削除
-  PLAN: HOST + "/plans/$1",
+  PLAN: API_HOST + "/plans/$1",
   // コメント一覧取得
-  PLAN_COMMENTS: HOST + "/plans/$1/comments",
+  PLAN_COMMENTS: API_HOST + "/plans/$1/comments",
   // お気に入り登録者一覧取得
-  PLAN_LIKES: HOST + "/plans/$1/likes"
+  PLAN_LIKES: API_HOST + "/plans/$1/likes"
+};
+
+/**
+ * WebViewエンドポイント
+ */
+export const WEB_ENDPOINT = {
+  // 利用規約
+  TERMS: WEB_HOST + "/webview/terms",
+  // プライバシーポリシー
+  PRIVACY_POLICY: WEB_HOST + "/webview/privacy"
 };
