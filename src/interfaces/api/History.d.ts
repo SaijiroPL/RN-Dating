e4f810090362518c27245507cc2849aa2e4e4ad4
Kota Tanaka <te11k.tanaka@gmail.com>
2019-10-02 16:17:50 +0900

    fix: 検索履歴インタフェース修正

--- src/interfaces/api/History.d.ts
+++ src/interfaces/api/History.d.ts
@@ -1,8 +1,18 @@
 /**
+ * 検索履歴リスト要素
+ * @author kotatanaka
+ */
+export interface IHistory {
+  history_id: number;
+  word: string;
+  search_date: string;
+}
+
+/**
  * 検索履歴一覧取得APIレスポンス
  * @author itsukiyamada
  */
 export interface IHistoryList {
   total: number;
-  history_list: Array<string>;
+  history_list: Array<IHistory>;
 }
