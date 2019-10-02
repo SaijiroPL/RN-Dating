1040a9e0ee74dddba549a5c97fbd76190dbb5d8a
Kota Tanaka <te11k.tanaka@gmail.com>
2019-10-02 16:18:49 +0900

    refactor: 検索履歴一覧画面レイアウト改修

--- src/screens/MyProfileScreen/DeleteHistoryScreen.tsx
+++ src/screens/MyProfileScreen/DeleteHistoryScreen.tsx
@@ -1,18 +1,23 @@
-import React, { useState } from "react";
-import { View, Text } from "native-base";
-import { SwipeListView } from "react-native-swipe-list-view";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Header, Text } from "native-base";
+import axios, { CancelTokenSource } from "axios";
+
 // from app
 import { IHistoryList } from "app/src/interfaces/api/History";
 import { IApiError } from "app/src/interfaces/api/Error";
+import { LoadingSpinner } from "app/src/components/Spinners";
+import HistorySwipeList from "app/src/components/lists/HistorySwipeList";
+import appTextStyle from "app/src/styles/GeneralTextStyle";
 
 /**
  * 検索履歴一覧画面
  * @author itsukiyamada
  */
 const DeleteHistoryScreen: React.FC = () => {
-  const [historys, sethistorys] = useState<IHistoryList>({
+  const [histories, setHistories] = useState<IHistoryList>({
     total: 0,
-    history_list: ["んぢいfs"]
+    history_list: []
   });
   const [errors, setErrors] = useState<IApiError>({
     code: 0,
@@ -21,32 +26,59 @@ const DeleteHistoryScreen: React.FC = () => {
   });
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
+  /** ライフサイクル */
+  useEffect(() => {
+    const signal = axios.CancelToken.source();
+    getHistoryList(signal);
+    return () => {
+      signal.cancel("Cancelling in Cleanup.");
+    };
+  }, []);
+
+  /** 検索履歴一覧取得 */
+  const getHistoryList = (signal: CancelTokenSource) => {
+    // TODO API繋ぎこみ
+
+    // FIXME APIを繋ぎこんだら下記サンプルデータは削除
+    const SAMPLE_HISTORIES: IHistoryList = {
+      total: 2,
+      history_list: [
+        {
+          history_id: 1,
+          word: "デート",
+          search_date: "xxx"
+        },
+        {
+          history_id: 2,
+          word: "おすすめ",
+          search_date: "xxx"
+        }
+      ]
+    };
+    setHistories(SAMPLE_HISTORIES);
+    setIsLoading(false);
+  };
+
+  /** 検索履歴削除 */
+  const deleteHistory = (id: number) => {
+    // TODO API繋ぎこみ
+  };
+
+  if (isLoading) {
+    return LoadingSpinner;
+  }
+
+  // TODO 件数を表示する?
   return (
-    <SwipeListView
-      data={historys.history_list}
-      renderItem={(data: { item: React.ReactNode }) => (
-        <View
-          style={{ backgroundColor: "gray", padding: 15, borderBottomWidth: 1 }}
-        >
-          <Text> {data.item}</Text>
-        </View>
-      )}
-      renderHiddenItem={(data, rowMap) => (
-        <View
-          style={{
-            flex: 1,
-            flexDirection: "row",
-            justifyContent: "flex-end",
-            backgroundColor: "red",
-            padding: 15,
-            borderBottomWidth: 1
-          }}
-        >
-          <Text>削除する</Text>
-        </View>
-      )}
-      rightOpenValue={-10}
-    />
+    <View>
+      <Header>
+        <Text style={appTextStyle.defaultText}>検索履歴一覧</Text>
+      </Header>
+      <HistorySwipeList
+        histories={histories.history_list}
+        onDelete={deleteHistory}
+      />
+    </View>
   );
 };
 
