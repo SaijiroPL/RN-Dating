597ae65ddc7708486d19a157afda15251f53c5ff
Kota Tanaka <te11k.tanaka@gmail.com>
2019-10-11 02:24:45 +0900

    feat: ラベル付き入力フォームのカスタムコンポーネント作成

---
+++ src/components/contents/InputLabelForm.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Input, Item, Label } from "native-base";
+
+// from app
+import Colors from "app/src/constants/Colors";
+import Layout from "app/src/constants/Layout";
+
+interface Props {
+  label: string;
+  value?: string;
+  numValue?: number;
+  setValue?: React.Dispatch<React.SetStateAction<string>>;
+  setNumValue?: React.Dispatch<React.SetStateAction<number>>;
+}
+
+/**
+ * テキスト入力フォーム(ラベル付き)
+ * @author kotatanaka
+ */
+const IuputLabelForm: React.FC<Props> = (props: Props) => {
+  const { label, value, numValue, setValue, setNumValue } = props;
+
+  /** 文字列 */
+  if (value && setValue && !numValue && !setNumValue) {
+    return (
+      <Item inlineLabel>
+        <Label style={thisStyle.label}>{label}</Label>
+        <Input
+          onChangeText={value => setValue(value)}
+          value={value}
+          style={thisStyle.input}
+        />
+      </Item>
+    );
+  }
+
+  /** 数値 */
+  if (!value && !setValue && numValue && setNumValue) {
+    return (
+      <Item inlineLabel>
+        <Label style={thisStyle.label}>{label}</Label>
+        <Input
+          onChangeText={numValue => setNumValue(+numValue)}
+          value={`${numValue}`}
+          style={thisStyle.input}
+        />
+      </Item>
+    );
+  }
+
+  return <Item />;
+};
+
+export default IuputLabelForm;
+
+/** スタイリング */
+const thisStyle = StyleSheet.create({
+  label: {
+    color: Colors.textTintColor,
+    fontFamily: "genju-medium",
+    fontSize: 14,
+    width: Layout.window.width * 0.25
+  },
+  input: {
+    fontFamily: "genju-light"
+  }
+});
