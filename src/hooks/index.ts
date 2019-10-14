f77dc5c24a1fe15435303f71aeec6b3342fe0aac
Kota Tanaka <te11k.tanaka@gmail.com>
2019-10-15 02:22:10 +0900

    feat: デートプラン詳細取得カスタムフック

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,7 @@
 export * from "./useGetPlanList";
 /** デートプラン検索 */
 export * from "./useSearchPlanList";
+/** デートプラン詳細取得 */
+export * from "./useGetPlanDetail";
+/** コメント一覧取得 */
+export * from "./useGetCommentList";
