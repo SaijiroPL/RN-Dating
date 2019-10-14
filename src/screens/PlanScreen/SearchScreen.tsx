808d1c64edd9d8cee0a8a276517d119f098daec0
Kota Tanaka <te11k.tanaka@gmail.com>
2019-10-15 01:36:28 +0900

    refactor: 検索画面のロジックとビューを分離

--- src/screens/PlanScreen/SearchScreen.tsx
+++ src/screens/PlanScreen/SearchScreen.tsx
@@ -1,89 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
-import axios from "axios";
 
 // from app
-import { useGlobalState } from "app/src/Store";
-import { API_ENDPOINT, COLOR } from "app/src/constants";
-import { IPlanList } from "app/src/interfaces/api/Plan";
-import { IApiError } from "app/src/interfaces/api/Error";
+import { COLOR } from "app/src/constants";
 import { Indicator, RefreshSpinner } from "app/src/components/Spinners";
 import { SearchFormBar } from "app/src/components/Form";
 import { PlanCardList } from "app/src/components/List";
-import { handleError } from "app/src/utils";
+import { useSearchPlanList } from "app/src/hooks";
 import { appTextStyle } from "app/src/styles";
 
 /**
- * プラン検索画面
+ * デートプラン検索画面
  * @author kotatanaka
  */
 const SearchScreen: React.FC = () => {
-  const loginUser = useGlobalState("loginUser");
-
-  const [searchWord, setSearchWord] = useState<string>("");
-  const [plans, setPlans] = useState<IPlanList>({
-    total: 0,
-    plan_list: []
-  });
-  const [errors, setErrors] = useState<IApiError>({
-    code: 0,
-    message: "",
-    detail_message: []
-  });
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [isRefreshing, setRefreshing] = useState<boolean>(false);
-
-  useEffect(() => {
-    const signal = axios.CancelToken.source();
-    return () => {
-      signal.cancel("Cancelling in Cleanup.");
-    };
-  }, []);
-
-  /** デートプラン検索API */
-  const searchPlanList = () => {
-    if (searchWord === "") {
-      return;
-    }
-
-    if (!isRefreshing) {
-      setIsLoading(true);
-    }
-
-    const signal = axios.CancelToken.source();
-    const url = API_ENDPOINT.PLANS_SEARCH;
-
-    axios
-      .get(url, {
-        params: {
-          keyword: searchWord,
-          user_id: loginUser.id
-        },
-        cancelToken: signal.token
-      })
-      .then((response: { data: IPlanList }) => {
-        setPlans(Object.assign(response.data));
-        setIsLoading(false);
-      })
-      .catch(error => {
-        if (axios.isCancel(error)) {
-          console.log("Request Cancelled: " + error.message);
-        } else {
-          handleError(error);
-          if (error.response.stats === 400) {
-            setErrors(error.response.data);
-          }
-          setIsLoading(false);
-        }
-      });
-  };
-
-  /** プルリロード */
-  const onRefresh = () => {
-    setRefreshing(true);
-    searchPlanList();
-    setRefreshing(false);
-  };
+  /** デートプラン検索 */
+  const {
+    isLoading,
+    searchWord,
+    setSearchWord,
+    searchPlanList,
+    plans,
+    errors,
+    isRefreshing,
+    onRefresh
+  } = useSearchPlanList();
 
   return (
     <View style={thisStyle.container}>
