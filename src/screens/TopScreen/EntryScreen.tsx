21953b48045f9fbc38988c72e4a8f769f555e151
Kota Tanaka <te11k.tanaka@gmail.com>
2019-10-22 15:14:23 +0900

    feat: ユーザー登録時ユーザー名のバリデーション

--- src/screens/TopScreen/EntryScreen.tsx
+++ src/screens/TopScreen/EntryScreen.tsx
@@ -37,7 +37,7 @@ const EntryScreen: React.FC = () => {
     prefecture,
     setPrefecture,
     createUser,
-    isLoading
+    errors
   } = useSignup();
 
   /** ログイン機能 */
@@ -55,13 +55,27 @@ const EntryScreen: React.FC = () => {
 
   /** 名前フォームの描画 */
   const renderNameForm = () => {
+    const nameErrors: Array<string> = [];
+    if (errors && errors.detail_message.length > 0) {
+      errors.detail_message.forEach(item => {
+        if (item.match(/Name/)) {
+          nameErrors.push(item.replace("Nameは", ""));
+        }
+      });
+    }
+
     return (
       <View style={thisStyle.formGroup}>
         <View style={{ marginRight: 20 }}>
           <Text style={thisStyle.entryText}>名前</Text>
         </View>
         <View style={{ width: LAYOUT.window.width * 0.4 }}>
-          <InputForm placeholder="" value={name} setValue={setName} />
+          <InputForm
+            placeholder=""
+            value={name}
+            setValue={setName}
+            errors={nameErrors}
+          />
         </View>
       </View>
     );
@@ -119,14 +133,10 @@ const EntryScreen: React.FC = () => {
 
   /** 入力完了ボタンの描画 */
   const renderCompleteButton = () => {
-    if (isLoading) {
-      return <View style={thisStyle.formGroup}>{LoadingSpinner}</View>;
-    }
-
     return (
       <View style={appStyle.emptySpace}>
         {/* 未入力項目がある場合はボタン押下不可 */}
-        {(isMan || isWoman) && prefecture.length > 0 ? (
+        {name && (isMan || isWoman) && prefecture ? (
           <CompleteButton title="決定" onPress={onCompleteButtonPress} />
         ) : (
           <CompleteButton title="決定" disabled />
