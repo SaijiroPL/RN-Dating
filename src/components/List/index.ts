3b28b5bb39451f6855ada8519c16bc9c10bce555
Kota Tanaka <te11k.tanaka@gmail.com>
2019-10-27 01:33:00 +0900

    feat: お知らせリストコンポーネント作成

--- src/components/List/index.ts
+++ src/components/List/index.ts
@@ -6,6 +6,10 @@ export * from "./CommentList";
 export * from "./FollowList";
 /** お気に入り登録者リスト */
 export * from "./LikeUserList";
+/** 通知リスト */
+export * from "./NotificationList";
+/** 運営からのお知らせリスト */
+export * from "./InformationList";
 /** 検索履歴リスト */
 export * from "./HistorySwipeList";
 /** 画像選択グリッドリスト */
