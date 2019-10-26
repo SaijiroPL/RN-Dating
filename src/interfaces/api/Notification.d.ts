ec54187a7cd94bf4c0d899fb07a04f216ceacba7
Kota Tanaka <te11k.tanaka@gmail.com>
2019-10-27 01:27:59 +0900

    feat: 通知参照系APIのインタフェース定義

---
+++ src/interfaces/api/Notification.d.ts
@@ -0,0 +1,46 @@
+/**
+ * 通知一覧取得APIレスポンス要素
+ * @author kotatanaka
+ */
+export interface INotification {
+  notification_id: string;
+  notification_category: NOTIFICATION_CATEGORY;
+  notification_date: string;
+  plan_id: string;
+  plan_title: string;
+  plan_comment: string;
+  user_id: string;
+  user_name: string;
+  user_image_url: string;
+  read: boolean;
+}
+
+/**
+ * 通知一覧取得APIレスポンス
+ * @author kotatanaka
+ */
+export interface INotificationList {
+  unread_count: number;
+  notification_list: Array<INotification>;
+}
+
+/**
+ * 運営からのお知らせ一覧取得APIレスポンス要素
+ * @author kotatanaka
+ */
+export interface IInformation {
+  notification_id: string;
+  notification_date: string;
+  title: string;
+  link: string;
+  read: boolean;
+}
+
+/**
+ * 運営からのお知らせ一覧取得APIレスポンス
+ * @author kotatanaka
+ */
+export interface IInformationList {
+  unread_count: number;
+  information_list: Array<IInformation>;
+}
