a70816a10fe1bf9b6c8115284927cd4e26952173
山田樹 <iy.s_s_c3s@icloud.com>
2020-03-26 10:23:07 +0900

    feat:できるところの配置終了

--- src/screens/CreatePlanScreen/ArrangeRouteScreen.tsx
+++ src/screens/CreatePlanScreen/ArrangeRouteScreen.tsx
@@ -17,7 +17,6 @@ import {
 import { useNavigation } from "react-navigation-hooks";
 
 // from app
-import { CompleteFooterButton } from "app/src/components/Button";
 import { ImageCarousel } from "app/src/components/Content";
 import { SimpleMapView } from "app/src/components/MapItem";
 import { useGetPlanDetail } from "app/src/hooks";
@@ -26,6 +25,7 @@ import { COLOR } from "app/src/constants";
 import { DatePicker } from "app/src/components/Form";
 import { getToday } from "app/src/utils";
 import { appTextStyle } from "app/src/styles";
+import { SmallCompleteButton } from "app/src/components/Button/SmallCompleteButton";
 
 /**
  * デートスポット順番並べ替え画面
@@ -42,34 +42,42 @@ const ArrangeRouteScreen: React.FC = () => {
   return (
     <Container>
       <Content />
-      <View style={thisStyle.formGroup}>
-        <Text style={thisStyle.itemTitleText}>デート予定日</Text>
-        <DatePicker date={date} setDate={setDate} minDate={getToday()} />
-      </View>
-      <Form>
-        <Item inlineLabel>
-          <Label>プラン名変更</Label>
-          <Input />
-        </Item>
-        <Item inlineLabel last>
-          <Label>ポイントを書く</Label>
-          <Input />
-        </Item>
-      </Form>
       {/**
       <ImageCarousel plan={plan} />
       <SimpleMapView spot={plan.spots[0]} />
        */}
-      <ListItem icon>
-        <Left>
-          <View style={thisStyle.columnTitle}>
-            <Text style={thisStyle.columnTitleText}>ポイント</Text>
+      <Text style={{ flex: 1 }}>ここに写真とルートを表示</Text>
+      <Form>
+        <Item inlineLabel>
+          <Text>
+            <Label style={thisStyle.text2}>プラン名変更</Label>
+          </Text>
+          <Input />
+          <View style={thisStyle.formGroup}>
+            <Text style={thisStyle.itemTitleText}>デート予定日</Text>
+            <DatePicker date={date} setDate={setDate} minDate={getToday()} />
           </View>
-        </Left>
-        <Body>ここに合計スポット滞在時間を表示</Body>
-        <Text style={thisStyle.itemTitleText}>デート予定日</Text>
-        {/**
-        <Right>
+        </Item>
+        <Item inlineLabel last style={{ marginTop: 25 }}>
+          <Text>
+            <Label style={thisStyle.text}>ポイントを書く</Label>
+          </Text>
+          <Input />
+        </Item>
+      </Form>
+      <View style={thisStyle.emptySpace}>
+        <ListItem icon>
+          <Left>
+            <View style={thisStyle.columnTitle}>
+              <Text style={thisStyle.columnTitleText}>スポット滞在時間</Text>
+            </View>
+          </Left>
+          <Body>
+            <Text style={thisStyle.text}>ここに合計スポット滞在時間を表示</Text>
+          </Body>
+          <Right>
+            <Text style={thisStyle.text}>ここに非公開ボタンを表示</Text>
+            {/**
           <Text style={thisStyle.itemTitleText}>投稿日時を非公開にする</Text>
           <Switch
             onValueChange={handleSwitchFacebookValue}
@@ -80,14 +88,19 @@ const ArrangeRouteScreen: React.FC = () => {
             onValueChange={handleSwitchFacebookValue}
             value={facebookOn}
           />
-        </Right>
         */}
-      </ListItem>
-      <CompleteFooterButton title="保存" onPress={onCompleteButtonPress} />
-      <CompleteFooterButton
-        title="保存して案内"
-        onPress={onCompleteButtonPress}
-      />
+          </Right>
+        </ListItem>
+      </View>
+      <View style={thisStyle.button1}>
+        <View style={{ marginLeft: 10 }} />
+        <SmallCompleteButton title="保存" onPress={onCompleteButtonPress} />
+        <View style={{ width: 20, marginRight: 10 }} />
+        <SmallCompleteButton
+          title="保存して案内"
+          onPress={onCompleteButtonPress}
+        />
+      </View>
     </Container>
   );
 };
@@ -96,7 +109,6 @@ const ArrangeRouteScreen: React.FC = () => {
 const thisStyle = StyleSheet.create({
   formGroup: {
     alignItems: "center",
-    flex: 1,
     flexDirection: "row"
   },
   columnTitle: {
@@ -114,7 +126,27 @@ const thisStyle = StyleSheet.create({
   itemTitleText: {
     color: COLOR.textTintColor,
     fontFamily: "genju-medium",
+    fontSize: 10,
     marginRight: 10
+  },
+  button1: {
+    alignItems: "center",
+    flexDirection: "row",
+    marginBottom: 5,
+    padding: 15
+  },
+  text: {
+    color: COLOR.textTintColor,
+    fontSize: 10,
+    padding: 20
+  },
+  text2: {
+    color: COLOR.textTintColor,
+    flexDirection: "row",
+    fontSize: 12
+  },
+  emptySpace: {
+    marginBottom: 5
   }
 });
 
