d30ab9e754b628c24b5516a50f6e7bd77b33729b
KotaTanaka <te11k.tanaka@gmail.com>
2020-04-06 15:20:44 +0900

    refactor: 最上位のナビゲーターをStackNavigatorに変更

--- src/navigators/TopNavigator.tsx
+++ src/navigators/TopNavigator.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 // from app
@@ -11,21 +10,23 @@ const TopStack = createStackNavigator();
 
 /** アプリトップ画面のナビゲーター */
 const TopNavigator: React.FC = () => (
-  <NavigationContainer>
-    <TopStack.Navigator>
-      <TopStack.Screen name="top" component={AppTopScreen} />
-      <TopStack.Screen
-        name="terms"
-        component={TermsScreen}
-        options={{ title: '利用規約' }}
-      />
-      <TopStack.Screen
-        name="privacy"
-        component={PrivacyPolycyScreen}
-        options={{ title: 'プライバシーポリシー' }}
-      />
-    </TopStack.Navigator>
-  </NavigationContainer>
+  <TopStack.Navigator>
+    <TopStack.Screen
+      name="top"
+      component={AppTopScreen}
+      options={{ headerShown: false }}
+    />
+    <TopStack.Screen
+      name="terms"
+      component={TermsScreen}
+      options={{ title: '利用規約' }}
+    />
+    <TopStack.Screen
+      name="privacy"
+      component={PrivacyPolycyScreen}
+      options={{ title: 'プライバシーポリシー' }}
+    />
+  </TopStack.Navigator>
 );
 
 export default TopNavigator;
