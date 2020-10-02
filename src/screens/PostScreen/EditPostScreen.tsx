b6609edbf12f4bad318b48afc3fcbd1c410b48a3
Trieu Niem <trieuniemit@gmail.com>
2020-10-02 17:09:49 +0700

    Done: 159, 160, 164

--- src/screens/PostScreen/EditPostScreen.tsx
+++ src/screens/PostScreen/EditPostScreen.tsx
@@ -1,5 +1,5 @@
-import React, { useCallback } from 'react';
-// import { StyleSheet } from 'react-native';
+import React from 'react';
+import { StyleSheet, ActionSheetIOS, TouchableOpacity } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import {
   Container,
@@ -7,7 +7,7 @@ import {
   Text,
   Left,
   Body,
-  // Switch,
+  Switch,
   View,
   Right,
   Form,
@@ -17,66 +17,135 @@ import {
 } from 'native-base';
 
 // from app
-// import { COLOR } from 'app/src/constants';
+import { COLOR } from 'app/src/constants';
 import { CompleteButton } from 'app/src/components/Button';
 import { appTextStyle } from 'app/src/styles';
+import { DateTimePickerLabel } from 'app/src/components/Form';
+import { useUpdatePost } from 'app/src/hooks/useUpdatePost';
 
 /** 投稿編集画面 */
 const EditPostScreen: React.FC = () => {
   const { params } = useRoute();
-  const { plan } = params;
+  const { goBack } = useNavigation();
 
-  const onCompleteButtonPress = useCallback(() => {
-    // navigate('Post');
-  }, []);
+  const {
+    title,
+    setTitle,
+    desc,
+    setDesc,
+    transportation,
+    setTransportation,
+    fromDate,
+    updateFrom,
+    updatePlan,
+    status,
+    setStatus,
+  } = useUpdatePost(params.plan);
+
+  const trans = { car: '車', train: '電車', bus: 'バス', walk: '徒歩' };
+
+  const showActionSheet = () => {
+    ActionSheetIOS.showActionSheetWithOptions(
+      {
+        options: ['Cancel', ...Object.values(trans)],
+        cancelButtonIndex: 0,
+      },
+      (buttonIndex) => {
+        if (buttonIndex !== 0) {
+          setTransportation(Object.keys(trans)[buttonIndex - 1]);
+        }
+      },
+    );
+  };
+
+  const onSave = async () => {
+    const res = await updatePlan();
+    if (res) {
+      goBack();
+    }
+  };
 
   return (
     <Container>
       <Content>
-        {/* TODO 投稿する画像を表示できるようにする */}
-        {/* TODO 投稿する場所をマップで表示する */}
         <Form>
           <Item fixedLabel>
-            {/* TODO ここにスポットの名前が自動で挿入されるようにする */}
             <Label>プラン名</Label>
-            <Input value={plan.title} />
+            <Input value={title} onChangeText={setTitle} />
           </Item>
-          <Item fixedLabel last>
-            <Label>プランの説明</Label>
-            <Input value={plan.description} />
+          <Item fixedLabel last style={{ alignItems: 'top' }}>
+            <Label style={{ marginTop: 10 }}>プランの説明</Label>
+            <Input
+              multiline
+              numberOfLines={3}
+              style={{ height: 90 }}
+              value={desc}
+              onChangeText={setDesc}
+            />
+          </Item>
+          <Item fixedLabel style={{ paddingRight: 15 }}>
+            <Left>
+              <Text style={thisStyle.label}>デート日</Text>
+            </Left>
+            <Body />
+            <Right>
+              <DateTimePickerLabel
+                date={fromDate}
+                setDate={updateFrom}
+                // minDate={getToday()}
+              />
+            </Right>
+          </Item>
+          <Item style={{ paddingTop: 15, paddingBottom: 15, paddingRight: 25 }}>
+            <Left>
+              <Label>交通手段リスト</Label>
+            </Left>
+            <Body />
+            <Right>
+              <TouchableOpacity onPress={showActionSheet}>
+                <Text>{trans[transportation]}</Text>
+              </TouchableOpacity>
+            </Right>
+          </Item>
+          <Item style={{ paddingTop: 15, paddingBottom: 15, paddingRight: 25 }}>
+            <Left>
+              <Label>プランの説明</Label>
+            </Left>
+            <Body />
+            <Right>
+              <Switch
+                onValueChange={(val) => setStatus(val ? 'private' : 'public')}
+                value={status === 'private'}
+              />
+            </Right>
           </Item>
         </Form>
-        <Left>
-          <Text style={appTextStyle.defaultText}>スポット滞在時間</Text>
-        </Left>
-        <Body>
-          {/* TODO スポット滞在時間を表示、変更もできるようにする */}
-          {/* <Text style={thisStyle.descriptionText}>
-            {formatDate(plan.date, "YYYY年MM月DD日TT時MM分")}
-          </Text> */}
-        </Body>
-        <Right>
-          {/* TODO 非公開ボタンを挿入 */}
-          {/* <Text>投稿を非公開にする</Text>
-          <Switch onValueChange={handleSwitchPrivateValue} value={privateOn} />
-          <Text>投稿を非公開にする</Text>
-          <Switch onValueChange={handleSwitchPrivateValue} value={privateOn} /> */}
-        </Right>
       </Content>
       <View style={{ alignItems: 'center' }}>
-        <CompleteButton title="投稿" onPress={onCompleteButtonPress} />
+        <CompleteButton title="投稿" onPress={onSave} />
       </View>
     </Container>
   );
 };
 
 /** スタイリング */
-// const thisStyle = StyleSheet.create({
-//   descriptionText: {
-//     color: COLOR.textTintColor,
-//     fontFamily: 'genju-light',
-//     fontSize: 10,
-//   },
-// });
+const thisStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 40,
+    alignItems: 'center',
+  },
+  descriptionText: {
+    color: COLOR.textTintColor,
+    fontFamily: 'genju-light',
+    fontSize: 10,
+  },
+  label: {
+    marginLeft: 0,
+    paddingTop: 15,
+    paddingBottom: 15,
+    ...appTextStyle.defaultText,
+  },
+});
 
 export default EditPostScreen;
