5930880cd05ba599efcb9b3b614e62a2d3459e26
Saijiro <pclienth@hotmail.com>
2020-12-11 18:32:05 +0900

    Fab Button TopRight

--- src/components/Button/CreateSpotFab.tsx
+++ src/components/Button/CreateSpotFab.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback } from 'react';
-import { Fab } from 'native-base';
+import { Fab, View } from 'native-base';
 import { useNavigation } from '@react-navigation/native';
 import { FontAwesome5 } from '@expo/vector-icons';
 
@@ -20,10 +20,19 @@ export const CreateSpotFab: React.FC = () => {
       active
       containerStyle={{}}
       style={{ backgroundColor: COLOR.tintColor }}
-      position="bottomRight"
+      position="topRight"
       onPress={toCreate}
     >
-      <FontAwesome5 name="map-marker" size={24} color={COLOR.greyColor} />
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}
+      >
+        <FontAwesome5 name="map-marker" size={24} color={COLOR.greyColor} />
+        <FontAwesome5 name="plus" size={12} color={COLOR.greyColor} />
+      </View>
     </Fab>
   );
 };
